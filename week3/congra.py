1ec3d9c184793343e9b38581eb20233a67ca9040
INE-02 <INE-02@Admins-iMac-5.local>
2020-08-04 14:39:55 +0700

    savefile

--- week3/congra.py
+++ week3/congra.py
@@ -1,7 +1,7 @@
 test1 = int(input('Enter the score for test 1: '))
 test2 = int(input('Enter the score for test 2: '))
 test3 = int(input('Enter the score for test 3: '))
-ave = (test1+test2+test3)/3
+ave = ( test1+test2+test3 )/3
 print('The average score is',ave)
 
 if ave > 95:
