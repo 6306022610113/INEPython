322d66b3e7661a444ca1a95985b26e00b0080b50
INE-02 <INE-02@Admins-iMac-5.local>
2020-08-11 14:51:24 +0700

    savefile

--- week4/while1.py
+++ week4/while1.py
@@ -1,15 +1,18 @@
+#This program calculates sales commissions.
+# Create a variable to control the loop.
 keep_going = 'y'
 
-while keep_going == 'y':
-
+# Calculate  a series of commissions.
+while keep_going == 'y' or keep_going == 'Y' :
+    #Get a salesperson's sales and sommission rate.
     sales = float(input('Enter the amount of sales: '))
     comm_rate = float(input('Enter the commission rate: '))
 
-
+    #Ca
     commission = sales * comm_rate
 
     print('The commmission is $',\
         format(commission, ',.2f'), sep='')
 
     keep_going = input('Do you want to calculate another' +  \
-        ' commission (Enter y for yes):')
+        ' commission (Enter y or Y for yes): ')
---
+++ week4/while2.py
@@ -0,0 +1,22 @@
+import random 
+
+print("What is my mad]gic number (1 or 100) ?")
+mynumber = random.randint(1,100)
+ntries = 1
+yourguess = -1
+while ntries < 7 and mynumber != yourguess :
+    msg = str(ntries) + ">> "
+    if(ntries == 6) :
+        print("Your last")
+    yourguess = int(input(msg))
+
+    if mynumber < yourguess :
+        print("--> too high")
+    else :
+        print("--> too low")
+    ntries += 1
+
+if yourguess == mynumber:
+    print("Yes! it's" ,mynumber)
+else :
+    print("Sorry! my number is" , mynumber)
