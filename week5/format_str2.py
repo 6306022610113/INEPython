c556d5d9d106a43cd20baa00d66662ee6ac0d1be
INE-02 <INE-02@Admins-iMac-5.local>
2020-08-18 15:03:53 +0700

    savefile

---
+++ week5/format_str2.py
@@ -0,0 +1,11 @@
+print(' {0:8}  |   {1:8} ' .format('Fruit' , 'Quantity'))
+print(' {0:8}  |   {1:8} ' .format('Apple' , '3'))
+print(' {0:8}  |   {1:8} ' .format('Orange' , '10'))
+
+#print(' {0:8}  |   {1:>8} ' .format('Fruit' , 'Quantity'))
+#print(' {0:8}  |   {1:>8} ' .format('Apple' , '3'))
+#print(' {0:8}  |   {1:>8} ' .format('Orange' , '10'))
+#ผลลัพธ์
+#Fruit      |       Quantity
+#Apple      |              3
+#Orange     |             10
