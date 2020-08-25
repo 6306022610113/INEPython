3ff593abbea7b468299fdc83302af90f1a75c332
INE-02 <INE-02@Admins-iMac-5.local>
2020-08-25 15:09:42 +0700

    savefile

--- week6/passargustr.py
+++ week6/passargustr.py
@@ -1,7 +1,8 @@
 #This program demontrates passing two string
 #argumant to a function.
 def reverse_name(first , last):
-    print(last, first)
+    print((''.join(reversed(last))),(''.join(reversed(first))))
+    print(last[::-1],first[::-1])
 
 def main():
     first_name = input('Enter your first name:')
