b50e3a253ee4d0da9afa79eef4335fa75812f722
INE-02 <INE-02@Admins-iMac-5.local>
2020-08-25 19:03:53 +0700

    savefile

--- week6/using_modules.py
+++ week6/using_modules.py
@@ -13,4 +13,32 @@ def main() :
         display_menu()
         choice = int(input('Enter your choice : '))
         if choice == AREA_CIRCLE_CHOICE:
-            radius = float(input('Enter the circle's ))
+            radius = float(input("Enter the circle's radius : "))
+            print('The area is ',circlr.area(radius))
+        elif choice == CIRCUMFERENCE_CHOICE :
+            radius = float(input("Enter the circle's radius : "))
+            print('The circumference is', \
+                circle.circumference(radius))
+        elif choice == AREA_RECTANGLE_CHOICE :
+            width = float(input("Enter the rectangle's radius : "))
+            length = float(input("Enter the rectangle's radius : "))
+            print('The area is', rectangle.area(width,length))
+        elif choice == PERIMETER_RECTANGLE_CHOICE:
+            width = float(input("Enter the rectangle's width : "))
+            length = float(input("Enter the rectangle's length : "))
+            print('The perimeter is', \
+                rectangle.perimeter(width, length))
+        elif choice == QUIT_CHOICE :
+            print('Exiting the program...')
+        else :
+            print('Error : invalid selection.')
+
+def display_menu():
+    print('         MENU')
+    print('1) Area of a circle')
+    print('2) Circumference of a circle')
+    print('3) Area of a rectangle')
+    print('4) Perimeter of a rectangle')
+    print('5) Quit')
+
+main()
--- week6/value_return1.py
+++ week6/value_return1.py
@@ -1,3 +1,8 @@
+def main():
+    first_name,last_name = get_name()
+    print('First name : ',first_name, 'Last name : ',last_name)
+    print(password2(first_name,last_name))
+
 def get_name():
     #Get the user's first and last names.
     first = input('Enter your first name : ')
@@ -5,5 +10,21 @@ def get_name():
     #Return both names.
     return first,last
 
-first_name,last_name = get_name()
-print('First name : ',first_name, 'Last name : ',last_name)
+def password(first,last):
+    d = len(last)//2
+    if len(last) % 2 == 0 :
+        print('password : ',first[:3:]+last[d-2:d+1])
+    else :
+        print('password : ',first[:3:]+last[d-1:d+2])
+
+def password2(first,last):
+    d = len(last)//2
+    if len(last) % 2 == 0 :
+        pas = first[:3:]+last[d-2:d+1]
+    else :
+        pas = first[:3:]+last[d-1:d+2]
+    return 'password : ' + pas 
+    
+
+    
+main()
